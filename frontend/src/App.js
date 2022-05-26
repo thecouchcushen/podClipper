0006f1c5de122b5eaddd3c0b531f0c1d74308665
Liam C <thecouchcushen@gmail.com>
2022-05-26 19:23:56 -0400

    Imported data (might need to restructure)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [clips, setClips] = useState([])
+  const [episodes, setEpisodes] = useState([])
+  const [shows, setShows] = useState([])
+  const [people, setPeople] = useState([])
+
+  useEffect(() => {
+
+    const clipsRequest = axios.get("http://localhost:3001/clips")
+    const episodesRequest = axios.get("http://localhost:3001/episodes")
+    const showsRequest = axios.get("http://localhost:3001/shows")
+    const peopleRequest = axios.get("http://localhost:3001/people")
+
+    axios.all([clipsRequest, episodesRequest, showsRequest, peopleRequest]).then((response) => {
+      
+      const clipsResponse = response[0]
+
+      const episodesResponse = response[1]
+
+      const showsResponse = response[2]
+
+      const peopleResponse = response[3]
+
+      setClips(clipsResponse)
+      setEpisodes(episodesResponse)
+      setShows(showsResponse)
+      setPeople(peopleResponse)
+
+    }
+    )
+    
+  }, [])
+
+  console.log("clips: ", clips)
+  console.log("episodes: ", episodes)
+  console.log("shows: ", shows)
+  console.log("people: ", people)
+
   return (
     <div className="App">
       <header className="App-header">
--- frontend/src/Clip.js
+++ frontend/src/Clip.js
@@ -4,7 +4,8 @@ const Clip = (props) => {
 
     return (
         <div className="clipPanel">
-            
+            <p>{props.startTime} - {props.endTime}</p>
+            <p>{props.notes}</p>
         </div>
     )
 }
